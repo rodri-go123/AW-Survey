dec0b028de8ac1b204b3afc45077a8bbf0248cdc
rodri-go123 <71586672+rodri-go123@users.noreply.github.com>
2021-04-01 13:47:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,30 @@
 const express = require('express');
+const firebase = require('firebase');
 const Datastore = require('nedb');
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAZ92A2VdvPh78MN453AKHy5vs7QRz7zKk",
+    authDomain: "aw-data.firebaseapp.com",
+    databaseURL: "https://aw-data-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "aw-data",
+    storageBucket: "aw-data.appspot.com",
+    messagingSenderId: "114765976712",
+    appId: "1:114765976712:web:960c2e7a8a5a9caa757b3d"
+  };
+
+firebase.initializeApp(firebaseConfig)
+let firebase_database = firebase.database()
+
+firebase_database.ref("customPath").set(obj, function(error) {
+    if (error) {
+      // The write failed...
+      console.log("Failed with error: " + error)
+    } else {
+      // The write was successful...
+      console.log("success")
+    }
+})
+
 const app = express();
 const port = process.env.PORT
 app.listen(port, () => {
